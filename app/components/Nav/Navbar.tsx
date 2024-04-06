a292012a57a9f0641ce56c0d3fbd96a032621db5
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-04-06 05:40:31 -0400

    Hotfix mobile

--- app/components/Nav/Navbar.tsx
+++ app/components/Nav/Navbar.tsx
@@ -3,28 +3,15 @@ import { Group, Title, UnstyledButton } from '@mantine/core';
 import Link from 'next/link';
 import './navbar.css';
 import Linky from '../Linky/linky';
+import { useMediaQuery } from '@mantine/hooks';
 
 export function Navbar() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth <= 768); 
-    };
-
-    checkIsMobile(); 
-
-    window.addEventListener('resize', checkIsMobile); 
-
-    return () => {
-      window.removeEventListener('resize', checkIsMobile); 
-    };
-  }, []);
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return( 
     <Group justify='space-between'>       
       {window.location.pathname === '/chat' && 
-        <Title mb={20} ml={isMobile ? 30 : 70} ta="left" >
+        <Title mb={20} ml={isMobile ? 30 : 60} ta="left" >
           Chat with <span style={{ color: '#01b7FF' }}>Linky</span>.
         </Title>
       }   
