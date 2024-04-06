2f7ef4f13c7a79129da03e3b98a95b5ba11c4808
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-04-06 01:46:45 -0400

    Add home "Linky" fix

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -7,9 +7,10 @@ export function Welcome() {
     <>
       <Title className={classes.title} ta="left" mt={100} ml={140} fz={130}>
         Hi, I'm {' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: '#01b7FF', to: '#01b7FF' }}>
+        <Text inherit variant="gradient" component="span" gradient={{ from: '#01b7FF', to: '#01b7FF' }} pr={3}>
           Linky
         </Text>
+        .
         <TextInput className={classes.searchUrl} size="xl" radius="lg" mt={30} placeholder='Paste your URL here' fw={500}/>
         
       </Title>
