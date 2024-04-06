935258ba678b837a5eab91f385e3fbc5b5a0bc09
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-04-06 04:12:59 -0400

    Fix Linky navbar icon

--- components/Linky/linky.tsx
+++ components/Linky/linky.tsx
@@ -4,7 +4,7 @@ import LinkyFace from './linky_face';
 
 
 const Linky = () => (
-  <div style={{ position: 'relative', width: '100px', height: '100px', marginTop: '15px', marginRight: '28px'}}>
+  <div style={{ position: 'relative', width: '100px', height: '100px', marginTop: '15px'}}>
     <LinkyBody indents={4} borderRadius={37} color={'#01b7ff'} />
     <div style={{ position: 'absolute', top: 0, left: 0 }}>
       <LinkyFace eyeSize={6} mouthType={'zigzag'} />
